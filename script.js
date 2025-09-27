97c345447ec788312de29b3d226499d055d6311f
klhe0002 <klhe0002@stud.kea.dk>
2025-09-27 14:22:27 +0200

    changes

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+const works = [
+  {
+    id: "pio",
+    title: "01 Pio Vinbar",
+    skills: ["HTML", "CSS", "JavaScript", "ASTRO", "FIGMA", "UX Research", "Premiere Pro", "Illustrator", "Prototyping", "Branding"],
+  },
+  {
+    id: "bareen",
+    title: "02 Bareen X Fujifilm",
+    skills: ["HTML", "CSS", "JavaScript", "ASTRO", "FIGMA", "UX Research", "Supabase", "Premiere Pro", "Illustrator", "Content Creation", "Photoshop", "Client Pitch", "Ideation & Concept Development"],
+  },
+  {
+    id: "misbrug",
+    title: "03 Misbrugsportalen",
+    skills: ["HTML", "CSS", "JavaScript", "ASTRO", "FIGMA", "UX Research", "Premiere Pro", "Illustrator", "Prototyping", "Branding", "Client Pitch"],
+  },
+  {
+    id: "kh",
+    title: "04 KH Art",
+    skills: ["Shopify", "META Ads", "Figma", "Adobe", "E-commerce", "Content Creation", "Graphic Design", "Branding & Visual Identity", "Project Management", "Entrepreneurship", "Customer Relations"],
+  },
+  {
+    id: "rubber",
+    title: "05 Rubberduck",
+    skills: ["HTML", "CSS", "JavaScript", "ASTRO", "FIGMA", "Redesign", "Illustrator", "Prototyping", "Photography & Photo Editing", "Client Pitch", "Branding", "Graphic Design", "SCRUM"],
+  },
+];
+
+const container = document.querySelector(".works");
+
+works.forEach((work) => {
+  // H2
+  const h2 = document.createElement("h2");
+  h2.id = work.id;
+  h2.innerHTML = `<span class="klara">${work.title.split(" ")[0]}</span> ${work.title.substring(3)}`;
+
+  // UL
+  const ul = document.createElement("ul");
+  ul.id = `ul_${work.id}`;
+
+  work.skills.forEach((skill) => {
+    const li = document.createElement("li");
+    li.innerHTML = `<div class="kompe">${skill}</div>`;
+    ul.appendChild(li);
+  });
+
+  container.appendChild(h2);
+  container.appendChild(ul);
+});
+
 const buttons = [
   { btn: document.querySelector("#pio"), video: document.querySelector("#pio_video"), ul: document.querySelector("#ul_pio") },
   { btn: document.querySelector("#bareen"), video: document.querySelector("#bareen_video"), ul: document.querySelector("#ul_bareen") },
